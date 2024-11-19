09f2fb702f6b5c225caea66e8f1fc10f4b77157d
muhgas49 <nowstander@gmail.com>
2024-11-19 07:51:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let sell = document.getElementById('sell');
+let search = document.getElementById('donee')
+
+sell.onclick=function(){
+    
+    setTimeout(function(){
+        window.open('Login/index.html','_blank')
+      },700);
+}
+
+
+let inp = document.getElementById('pop');
+
+
